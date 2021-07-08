43c01bdf1d8ab19ba4c20108749a4638c1080ec4
ProDesert22 <pedroantonioppms@gmail.com>
2021-07-07 22:42:51 -0400

    Delete DEFI

--- rotkehlchen/chain/avalanche/node_avalanche_web3.py
+++
@@ -1,7 +0,0 @@
-from web3 import Web3, HTTPProvider
-
-class Node_Avalanche_Web3():
-    def __init__(self,rpc_endpoint):
-        self.rpc_endpoint = rpc_endpoint
-        w3 = Web3(HTTPProvider(self.rpc_endpoint))
-        
